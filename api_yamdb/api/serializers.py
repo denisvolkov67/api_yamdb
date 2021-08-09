95d7c1072ec6a5479130c8e61b181ff82d133bbe
Nikolai <smirnovnikolai@yandex.ru>
2021-08-09 23:28:04 +0300

    Доработка моделей, сериализаторов, вьюсетов и роутеров

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,4 @@
-from .models import Categories
+from .models import Categories, Genres, Title
 from rest_framework import serializers
 
 
@@ -7,3 +7,17 @@ class CategoriesSerializer(serializers.ModelSerializer):
         model = Categories
         fields = '__all__'
         read_only_fields = ('name',)
+
+
+class GenresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genres
+        fields = '__all__'
+        read_only_fields = ('name',)
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Title
+        fields = '__all__'
+        read_only_fields = ('genres', 'category')
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -128,7 +128,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 AUTH_USER_MODEL = 'users.User'
