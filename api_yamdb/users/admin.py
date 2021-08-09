e4f97bd257b2f32afd9879676eb40049a370b37d
Denis <DenisVolkov67@gmail.com>
2021-08-09 20:53:51 +0300

    update user model

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'username', 'role', 'first_name', 'last_name', 'email', 'bio')
+    search_fields = ('username',)
+    empty_value_display = '-пусто-'
+
+admin.site.register(User, UserAdmin)
